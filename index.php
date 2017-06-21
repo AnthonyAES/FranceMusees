5db93f76b4741c51afe087351078e05216636eaa
claire <claire.giacomel@gmail.com>
2017-06-21 17:07:59 +0200

    nouveautés maquettage

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <!-- METAS -->
+        <meta charset="UTF-8">
+        <title>Annuaire des Musées de France</title>
+        <meta name="description" content="annuaire gratuit et libre de tous les musées de France, pour simplifier vos recherches culturelles" />
+        <meta name="keywords" content="musée, musées, annuaire, liste, culture, culturel, visite, expositions, nouveautés" />
+        <meta name="abstract" content="annuaire gratuit et libre de tous les musées de France, pour simplifier vos recherches culturelles" />
+        <meta name="revisit-after" content="3 days" />
+        <meta name="generator" content="www.absolute-referencement.com">
+        <meta name="language" content="fr" />
+        <meta name="robots" content="All" />
+
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+
+
+        <!-- TYPO -->
+        <link href="https://fonts.googleapis.com/css?family=Didact+Gothic" rel="stylesheet">
+    </head>
+
+
+    <body class="container-fluid">
+
+
+        <!-- include header.php -->
+
+
+        <article class="row image-search">
+
+            <h3 class="col-xs-offset-1 col-xs-10 col-md-offset-3 col-md-6">Trouvez ici facilement le musée que vous cherchiez</h3>
+            <input class="search-box col-xs-offset-1 col-xs-10 col-md-offset-3 col-md-6" />
+
+        </article>
+
+        <article class="row find-museum">
+
+            <div class="square-title">
+                <div class="square"></div>
+                <h3 class="col-xs-offset-1 col-xs-10 col-md-offset-1 col-md-3">Liste des musées</h3>
+            </div>
+
+        </article>
+
+
+
+        <!-- include footer.php -->
+        <!--
+<script src="js/bootstrap.min.js"></script>-->
+        <script src="js/script.js"></script>
+    </body>
+</html>
