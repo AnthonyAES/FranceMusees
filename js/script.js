b33dd14d59511e01d54ced226815c1f7566fdfe4
AnthonyS <asautebin.pro@gmail.com>
2017-06-23 13:45:23 +0200

    map interactive with list

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var map = document.querySelector('.carte');
-var paths = map.querySelectorAll('.carte svg a');
-var links = map.querySelectorAll('.panel-body li');
+var paths = document.querySelectorAll('.carte>svg>a>path');
+var groups = document.querySelectorAll('.carte g.path-N');
+var links = document.querySelectorAll('.panel-body>ul>li>a');
 
 //Polyfill du foreach
 if (NodeList.prototype.forEach === undefined) {
@@ -9,40 +10,111 @@ if (NodeList.prototype.forEach === undefined) {
     }
 }
 
-var activeArea = function(id) {
+var activeArea = function(id, id2) {
     map.querySelectorAll('.is-active').forEach(function (item){
             item.classList.remove('is-active');
         });
     if (id !== undefined) {
-        document.querySelector('.path-' + id).classList.add('is-active');
-        document.querySelector('#region-' + id).classList.add('is-active');
-        console.log(id);
+        var fullId = '#FR-' + id + "-" + id2;
+        document.querySelector(fullId).classList.add('is-active');
+        document.querySelector('#region-'+id2).classList.add('is-active');
+        document.querySelector('#list-'+id).classList.add('is-active');
+    }
+}
+
+var activeArea2 = function(id, id2) {
+    map.querySelectorAll('.is-active').forEach(function (item){
+            item.classList.remove('is-active');
+        });
+    if (id !== undefined) {
+        var fullId = '#FR-' + id + "-" + id2 + ">path";
+        document.querySelector(fullId).classList.add('is-active');
+        document.querySelector(fullId+":nth-child(2)").classList.add('is-active');
+        document.querySelector('#region-'+id2).classList.add('is-active');
+        document.querySelector('#list-'+id+'-'+id2).classList.add('is-active');
+    }
+}
+
+/*var activeArea = function(id, id2) {
+    map.querySelectorAll('.is-active').forEach(function (item){
+            item.classList.remove('is-active');
+        });
+    if (id !== undefined) {
+        var fullId = '#FR-' + id + "-" + id2;
+        document.querySelector('path'+fullId).classList.add('is-active');
+        document.querySelector('#region-'+id2).classList.add('is-active');
+        document.querySelector('#list-'+id+'-'+id2).classList.add('is-active');
+    }
+}*/
+
+var activeArea4 = function(id, id2) {
+    map.querySelectorAll('.is-active').forEach(function (item){
+            item.classList.remove('is-active');
+        });
+    if (id !== undefined) {
+        var id1 = id.slice(5, 7);
+        var id2 = id.slice(8, 10);
+        var fullId = '#FR-' + id1 + "-" + id2;
+        var g = document.querySelector('g.path-N');
+        document.querySelector(fullId).classList.add('is-active');
+        if("#"+g.id == fullId){
+            document.querySelector(fullId+">path").classList.add('is-active');  
+            document.querySelector(fullId+">path:nth-child(2)").classList.add('is-active');  
+        }else {
+            
+        }
     }
 }
 
 paths.forEach(function (path) {
     path.addEventListener('mouseenter', function() {
-        var id = this.children[0].classList[1].replace('path-','');
-        console.log(id);
-        activeArea(id);
-    });
-});
-
-links.forEach(function (link) {
-    link.addEventListener('mouseenter', function() {
-        var id = this.id.replace('region-','');
-        activeArea(id);
+        var idLong = path.id;
+        var id = idLong.slice(3, 5);
+        var id2 = idLong.slice(-1);
+        activeArea(id, id2);
     });
 });
 
 paths.forEach(function (path) {
     path.addEventListener('mouseleave', function() {
+        var idLong = path.id;
+        var id = idLong.slice(3, 5);
+        var id2 = idLong.slice(-1);
         activeArea();
+        document.querySelector('#region-'+id2).classList.remove('is-active');
+        document.querySelector('#list-'+id+"-"+id2).classList.remove('is-active');
+    });
+});
+
+groups.forEach(function (group) {
+    group.addEventListener('mouseenter', function() {
+        var idLong = group.id;
+        var id = idLong.slice(3, 5);
+        var id2 = idLong.slice(-1);
+        activeArea2(id, id2);
+    });
+});
+
+groups.forEach(function (group) {
+    group.addEventListener('mouseleave', function() {
+        var idLong = group.id;
+        var id = idLong.slice(3, 5);
+        var id2 = idLong.slice(-1);
+        activeArea2();
+        document.querySelector('#region-'+id2).classList.remove('is-active');
+        document.querySelector('#list-'+id+'-'+id2).classList.remove('is-active');
+    });
+});
+
+links.forEach(function (link) {
+    link.addEventListener('mouseenter', function() {
+        var id = link.id;;
+        activeArea4(id);
     });
 });
 
 links.forEach(function (link) {
     link.addEventListener('mouseleave', function() {
-        activeArea();
+        activeArea4();
     });
 });
